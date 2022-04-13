ea1a87cbebcf4d8742d61129b6156312467da0bb
Andrews Matsubara <andrewsmatsubara@gmail.com>
2022-04-13 18:47:41 -0300

    Add cat api functionality

--- index.js
+++ index.js
@@ -1,15 +1,35 @@
 require('dotenv').config();
 
+const fetch = require('node-fetch');
+
 const Discord = require('discord.js');
-const client = new Discord.Client({intents: ["GUILDS", "GUILD_MESSAGES"]});
+
+const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });
+
+const getCatPicture = async () => {
+  const result = await fetch('https://api.thecatapi.com/v1/images/search');
+  const json = await result.json();
+  const url = await json[0].url;
+
+  return url;
+}
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`)
 });
 
-client.on('message', msg => {
+client.on('messageCreate', async msg => {
   if (msg.content === 'ping') {
     msg.reply('pong');
+  } else if (msg.content === '!Olá, Robô') {
+    msg.reply(`Olá, ${msg.author}!`);
+  } else if (msg.content === '!Adeus, Robô') {
+    msg.reply(`Adeus, ${msg.author}!`);
+  } else if (msg.content === '!cat') {
+    msg.channel.send({
+      content: 'Um gatinho para alegrar o seu dia!',
+      files: [await getCatPicture()]
+    });
   }
 });
 
