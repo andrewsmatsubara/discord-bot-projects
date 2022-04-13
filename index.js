27208b33fd986799de9ffe9ef91d8b9b113c3926
Andrews Matsubara <andrewsmatsubara@gmail.com>
2022-04-13 05:36:16 -0300

    Add intents on Discord.Client

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 const Discord = require('discord.js');
-const client = new Discord.Client();
+const client = new Discord.Client({intents: ["GUILDS", "GUILD_MESSAGES"]});
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`)
